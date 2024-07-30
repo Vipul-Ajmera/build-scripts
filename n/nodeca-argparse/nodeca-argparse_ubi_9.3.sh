32c38cd45f3c71778431a7ad98a186f340e58d94
Vipul-Ajmera <Vipul.Ajmera@ibm.com>
2024-07-30 14:45:35 +0530

    Create nodeca-argparse_ubi_9.3.sh

---
+++ n/nodeca-argparse/nodeca-argparse_ubi_9.3.sh
@@ -0,0 +1,61 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : nodeca-argparse
+# Version          : 2.0.1
+# Source repo      : https://github.com/nodeca/argparse.git
+# Tested on        : UBI:9.3
+# Language         : JavaScript
+# Travis-Check     : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Vipul Ajmera <Vipul.Ajmera@ibm.com>
+#
+# Disclaimer       : This script has been tested in root mode on given
+# ==========         platform using the mentioned version of the package.
+#                    It may not work as expected with newer versions of the
+#                    package and/or distribution. In such case, please
+#                    contact "Maintainer" of this script.
+#
+# ---------------------------------------------------------------------------
+
+#variables
+PACKAGE_NAME="argparse"
+PACKAGE_VERSION=${1:-2.0.1}
+PACKAGE_URL="https://github.com/nodeca/argparse.git"
+export NODE_VERSION=${NODE_VERSION:-20}
+
+#install dependencies
+sudo yum install -y python3 python3-devel.ppc64le git gcc gcc-c++ libffi make
+
+#Installing nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+source "$HOME"/.bashrc
+echo "installing nodejs $NODE_VERSION"
+nvm install "$NODE_VERSION" >/dev/null
+nvm use $NODE_VERSION
+
+#clone repository
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+#install
+if ! npm install && npm audit fix && npm audit fix --force; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+#test
+if ! npm test; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
