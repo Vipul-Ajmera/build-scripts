43213565dcb8aba059b67bfed7f39e9c5a199f7c
Vipul-Ajmera <Vipul.Ajmera@ibm.com>
2024-11-04 21:57:18 +0530

    Update build_script_python_create_wheel.sh

--- script/build_script_python_create_wheel.sh
+++ script/build_script_python_create_wheel.sh
@@ -57,7 +57,7 @@ install_python_version() {
             tar xzf Python-3.13.0rc1.tgz
             cd Python-3.13.0rc1
             ./configure --prefix=/usr/local --enable-optimizations
-            make -j 1
+            make -j 4
             make altinstall
             cd .. && rm -rf Python-3.13.0rc1.tgz
         fi
