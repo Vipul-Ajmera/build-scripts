ecca42be69bc2aaa25a23e9c111424be182fa3fa
Vipul-Ajmera <Vipul.Ajmera@ibm.com>
2024-11-07 11:27:10 +0530

    Update build_script_python_create_wheel.sh

--- script/build_script_python_create_wheel.sh
+++ script/build_script_python_create_wheel.sh
@@ -32,10 +32,10 @@ install_python_version() {
     "3.10")
         if ! python3.10 --version &>/dev/null; then
             echo "Now installing python3.10..."
-            wget https://www.python.org/ftp/python/3.10.14/Python-3.10.14.tgz
-            tar xf Python-3.10.14.tgz
+            wget https://www.python.org/ftp/python/3.10.15/Python-3.10.15.tgz
+            tar xf Python-3.10.15.tgz
             echo "Building python3.10..."
-            cd Python-3.10.14
+            cd Python-3.10.15
             echo "Still building..."
             ./configure --prefix=/usr/local --enable-optimizations
             echo "Still building..."
