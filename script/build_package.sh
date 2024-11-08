ed657b4c278aed5ff6e4cb8ceb9a8be204b865d4
Vipul-Ajmera <Vipul.Ajmera@ibm.com>
2024-11-08 10:28:15 +0530

    Update build_package.sh

--- script/build_package.sh
+++ script/build_package.sh
@@ -1,73 +1,74 @@
- #!/bin/bash -e
+#!/bin/bash -e
 
 sudo apt update -y && sudo apt-get install file -y
 #pip3 install --upgrade requests
 pip3 install --force-reinstall -v "requests==2.31.0"
 pip3 install --upgrade docker
 
-echo "Running build script execution in background for "$PKG_DIR_PATH$BUILD_SCRIPT" "$VERSION" " 
+echo "Running build script execution in background for "$PKG_DIR_PATH$BUILD_SCRIPT" "$VERSION" "
 echo "*************************************************************************************"
 
 docker_image=""
 
-# the below function is used for building a custom docker image, it will be called only when non root user build is set to true.
-# function accepts one argument, which is the base image value.
+# Function to build a custom Docker image for non-root users.
 docker_build_non_root() {
-  echo "building docker image for non root user build"
-  docker build --build-arg BASE_IMAGE="$1" -t docker_non_root_image -f script/dockerfile_non_root .
-  docker_image="docker_non_root_image"
+    echo "building docker image for non-root user build"
+    docker build --build-arg BASE_IMAGE="$1" -t docker_non_root_image -f script/dockerfile_non_root .
+    docker_image="docker_non_root_image"
 }
 
-#Below conditions are used to select the base image based on the 2 flags, tested_on and non_root_build. A docker_build_non_root function is called when non root build is true.
-if [[ "$TESTED_ON" == UBI:9*  || "$TESTED_ON" == UBI9* ]];
-then
+# Select base image based on `TESTED_ON` and `NON_ROOT_BUILD` flags.
+if [[ "$TESTED_ON" == UBI:9* || "$TESTED_ON" == UBI9* ]]; then
     docker pull registry.access.redhat.com/ubi9/ubi:9.3
     docker_image="registry.access.redhat.com/ubi9/ubi:9.3"
-    if [[ "$NON_ROOT_BUILD" == "true" ]];
-    then
-        docker_build_non_root "registry.access.redhat.com/ubi9/ubi:9.3"
-    fi
+    [[ "$NON_ROOT_BUILD" == "true" ]] && docker_build_non_root "registry.access.redhat.com/ubi9/ubi:9.3"
 else
     docker pull registry.access.redhat.com/ubi8/ubi:8.7
     docker_image="registry.access.redhat.com/ubi8/ubi:8.7"
-    if [[ "$NON_ROOT_BUILD" == "true" ]];
-    then
-        docker_build_non_root "registry.access.redhat.com/ubi8/ubi:8.7"
-    fi    
+    [[ "$NON_ROOT_BUILD" == "true" ]] && docker_build_non_root "registry.access.redhat.com/ubi8/ubi:8.7"
 fi
 
-python3 script/validate_builds_currency.py "$PKG_DIR_PATH$BUILD_SCRIPT" "$VERSION" "$docker_image" > build_log &
+# Function to run validation scripts and handle logging.
+run_and_log() {
+    local -a script_args=($1) # Split the script arguments into an array
+    local log_file="$2"
 
-SCRIPT_PID=$!
-while ps -p $SCRIPT_PID > /dev/null
-do 
-  echo "$SCRIPT_PID is running"
-  sleep 100
-done
-wait $SCRIPT_PID
-my_pid_status=$?
-build_size=$(stat -c %s build_log)
+    python3 script/validate_builds_currency.py "${script_args[@]}" >"$log_file" &
+    local pid=$!
 
-if [ $my_pid_status != 0 ];
-then
-    echo "Script execution failed for "$PKG_DIR_PATH$BUILD_SCRIPT" "$VERSION" "
-    echo "*************************************************************************************"
-    if [ $build_size -lt 1800000 ];
-    then
-       cat build_log
+    # Monitor the process
+    while ps -p $pid >/dev/null; do
+        echo "$pid is running"
+        sleep 100
+    done
+    wait $pid
+    local status=$?
+    local log_size=$(stat -c %s "$log_file")
+
+    if [ $status -ne 0 ]; then
+        echo "Script execution failed for "$PKG_DIR_PATH$BUILD_SCRIPT" "$VERSION" "
+        echo "*************************************************************************************"
+        if [ $log_size -lt 1800000 ]; then
+            cat "$log_file"
+        else
+            tail -100 "$log_file"
+        fi
+        exit 1
     else
-       tail -100 build_log
+        echo "Script execution completed successfully for "$PKG_DIR_PATH$BUILD_SCRIPT" "$VERSION" "
+        echo "*************************************************************************************"
+        if [ $log_size -lt 1800000 ]; then
+            cat "$log_file"
+        else
+            tail -100 "$log_file"
+        fi
     fi
-    exit 1
-else
-    echo "Script execution completed successfully for "$PKG_DIR_PATH$BUILD_SCRIPT" "$VERSION" "
-    echo "*************************************************************************************"
-    if [ $build_size -lt 1800000 ];
-    then
-       cat build_log
-    else
-       tail -100 build_log
-    fi    
-fi
-exit 0
+}
 
+# Run the required validation scripts
+[[ "$VALIDATE_BUILD_SCRIPT" == "true" ]] && run_and_log "$PKG_DIR_PATH$BUILD_SCRIPT $VERSION $docker_image" "validate_build_log"
+
+WHEEL_SCRIPT=script/build_script_python_create_wheel.sh
+[[ "$WHEEL_BUILD" == "true" ]] && run_and_log "$WHEEL_SCRIPT $PYTHON_VERSION $PKG_DIR_PATH$BUILD_SCRIPT $VERSION $docker_image" "wheel_build_log"
+
+exit 0
