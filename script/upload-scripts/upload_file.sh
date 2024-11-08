e689ddbb5dcbdf4336cffc6b1334e607bb967c8d
Vipul-Ajmera <Vipul.Ajmera@ibm.com>
2024-11-08 09:57:13 +0530

    Update upload_file.sh

--- script/upload-scripts/upload_file.sh
+++ script/upload-scripts/upload_file.sh
@@ -1,19 +1,31 @@
 #!/bin/bash -e
 
-token_request=$(curl -X POST https://iam.cloud.ibm.com/identity/token \
-  -H "content-type: application/x-www-form-urlencoded" \
-  -H "accept: application/json" \
-  -d "grant_type=urn%3Aibm%3Aparams%3Aoauth%3Agrant-type%3Aapikey&apikey=$travis_currency_service_id_api_key")
+# Check if the build is validate_build_script or wheel_build
+if [[ "$validate_build_script" == "true" ]]; then
+    api_key="$travis_currency_service_id_api_key" # API key for validate_build_script
+    content_type="application/gzip"
+    s3_url="https://s3.au-syd.cloud-object-storage.appdomain.cloud/currency-automation-toolci-bucket" # URL for validate_build_script
+elif [[ "$wheel_build" == "true" ]]; then
+    api_key="$currency_ecosystem_dev_service_api_key" # API key for wheel build
+    content_type="application/octet-stream"
+    s3_url="https://s3.us-east.cloud-object-storage.appdomain.cloud/currency-artifacts" # URL for wheel_build
+else
+    echo "Error: Neither validate_build_script nor wheel_build is true."
+    exit 1
+fi
 
-#token=$(echo "$token_request" | jq -r '.access_token')
-#curl -X PUT -H "Authorization: bearer $token" -H "Content-Type: application/gzip" -T $1 "https://s3.au-syd.cloud-object-storage.appdomain.cloud/currency-automation-toolci-bucket/$PACKAGE_NAME/$VERSION/$1"
+# Get the token request using the selected API key
+token_request=$(curl -X POST https://iam.cloud.ibm.com/identity/token \
+    -H "content-type: application/x-www-form-urlencoded" \
+    -H "accept: application/json" \
+    -d "grant_type=urn%3Aibm%3Aparams%3Aoauth%3Agrant-type%3Aapikey&apikey=$api_key")
 
 # Check if the token request was successful based on the presence of 'errorCode'
 if [[ $(echo "$token_request" | jq -r '.errorCode') == "null" ]]; then
     token=$(echo "$token_request" | jq -r '.access_token')
-    
+
     # curl command for uploading the file
-    response=$(curl -X PUT -H "Authorization: bearer $token" -H "Content-Type: application/gzip" -T $1 "https://s3.au-syd.cloud-object-storage.appdomain.cloud/currency-automation-toolci-bucket/$PACKAGE_NAME/$VERSION/$1")
+    response=$(curl -X PUT -H "Authorization: bearer $token" -H "Content-Type: $content_type" -T $1 "$s3_url/$PACKAGE_NAME/$VERSION/$1")
 
     # Check if the PUT request was successful based on the absence of an <Error> block
     if ! echo "$response" | grep -q "<Error>"; then
@@ -22,7 +34,7 @@ if [[ $(echo "$token_request" | jq -r '.errorCode') == "null" ]]; then
         # Handle PUT request failure
         echo "Error: PUT request failed. Response: $response"
         exit 1
-    fi    
+    fi
 else
     # Handle token request failure
     echo "Error: Token request failed. Response: $token_request"
