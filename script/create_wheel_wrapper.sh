6d0cbf4ae6be947c5eb891ac8492d67478b02a16
Vipul-Ajmera <Vipul.Ajmera@ibm.com>
2024-12-13 15:52:36 +0530

    Update create_wheel_wrapper.sh

--- script/create_wheel_wrapper.sh
+++ script/create_wheel_wrapper.sh
@@ -64,28 +64,24 @@ install_python_version() {
 # Install the specified Python version
 install_python_version "$PYTHON_VERSION"
 
-# Function to check for setup.py or *.toml files in a directory
-check_files_in_directory() {
-    local dir=$1
-
-    if [ -f "$dir/setup.py" ] || ls "$dir"/*.toml 1>/dev/null 2>&1; then
-        return 0
-    fi
-    return 1
-}
-
 # Function to copy and format the build script
 format_build_script() {
     if [ -n "$BUILD_SCRIPT_PATH" ]; then
         cp "$BUILD_SCRIPT_PATH" "$TEMP_BUILD_SCRIPT_PATH"
 
         # Modify the build script for compatibility
+        sed -i 's/\bpython[0-9]\+\.[0-9]\+ -m pip /pip /g' "$TEMP_BUILD_SCRIPT_PATH"
+        sed -i 's/python[0-9]\+\.[0-9]\+/python/g' "$TEMP_BUILD_SCRIPT_PATH"
         sed -i 's/python3 /python /g' "$TEMP_BUILD_SCRIPT_PATH"
         sed -i 's/pip3 /pip /g' "$TEMP_BUILD_SCRIPT_PATH"
-        sed -i 's/\bpython[0-9]\+\.[0-9]\+ -m pip /pip /g' "$TEMP_BUILD_SCRIPT_PATH"
         sed -i '/-m venv/d' "$TEMP_BUILD_SCRIPT_PATH"
         sed -i '/bin\/activate/d' "$TEMP_BUILD_SCRIPT_PATH"
         sed -i '/^deactivate$/d' "$TEMP_BUILD_SCRIPT_PATH"
+        sed -i '/yum install/{s/\b\(python\|python-devel\|python-pip\)\b[[:space:]]*//g}' "$TEMP_BUILD_SCRIPT_PATH"
+        sed -i '/dnf install/{s/\b\(python\|python-devel\|python-pip\)\b[[:space:]]*//g}' "$TEMP_BUILD_SCRIPT_PATH"
+        sed -i 's/\bpython3 -m pytest/pytest/g' "$TEMP_BUILD_SCRIPT_PATH"
+        sed -i "s/tox -e py[0-9]\{3\}/tox -e py${PYTHON_VERSION//./}/g" "$TEMP_BUILD_SCRIPT_PATH"
+        
     else
         echo "No build script specified, skipping copying."
     fi
@@ -123,55 +119,37 @@ echo "=============== Running package build-script starts =================="
 
 if [ -n "$TEMP_BUILD_SCRIPT_PATH" ]; then
     python$PYTHON_VERSION -m pip install --upgrade pip setuptools wheel build pytest nox tox
-      
-    package_url=$(grep -oP '(?<=^PACKAGE_URL=).*' "$TEMP_BUILD_SCRIPT_PATH" | tr -d '"')
-    package_name=$(basename "$package_url" .git)
-    
+
+    package_dir=$(grep -oP '(?<=^PACKAGE_DIR=).*' "$TEMP_BUILD_SCRIPT_PATH" | tr -d '"')
 
     sh "$TEMP_BUILD_SCRIPT_PATH" $EXTRA_ARGS
+
+    # Check if the build script execution was successful
+    if [ $? -ne 0 ]; then
+        echo "Build script execution failed. Skipping wheel creation."
+        cleanup "$VENV_DIR"
+        [ -n "$TEMP_BUILD_SCRIPT_PATH" ] && rm "$CURRENT_DIR/$TEMP_BUILD_SCRIPT_PATH"
+        exit 1
+    fi
+
 else
     echo "No build script to run, skipping execution."
 fi
 
-echo "=============== Running package build-script ends =================="
-
-# Check if the build script execution was successful
-if [ $? -ne 0 ]; then
-    echo "Build script execution failed. Skipping wheel creation."
-    cleanup "$VENV_DIR"
-    [ -n "$TEMP_BUILD_SCRIPT_PATH" ] && rm "$CURRENT_DIR/$TEMP_BUILD_SCRIPT_PATH"
-    exit 1
-fi
-
 # Navigate to the package directory
 echo "Navigating to the package directory"
-cd $package_name
+cd $package_dir
 
 echo "=============== Building wheel =================="
 
-# Check for setup.py or *.toml files in the current directory or subdirectories
-if [ ! -f "setup.py" ] && ! ls *.toml 1>/dev/null 2>&1; then
-    echo "setup.py or *.toml not found in the current directory. Checking subdirectories..."
-    dir=$(find . -type f -name "setup.py" -o -name "*.toml" -print -quit | xargs -I {} dirname "{}")
-
-    if [ -n "$dir" ]; then
-        echo "setup.py or *.toml found in $dir"
-        cd "$dir"
-    else
-        echo "No setup.py or *.toml found in any subdirectory."
-    fi
-else
-    echo "========= setup.py or *.toml found in the package directory ========="
-fi
-
-# Attempt to build the wheel with isolation
-if ! python -m build --wheel --outdir="$CURRENT_DIR/"; then
-    echo "============ Wheel Creation Failed for Python $PYTHON_VERSION ================="
-    echo "Attempting to build without isolation..."
+# Attempt to build the wheel without isolation
+if ! python -m build --wheel --no-isolation --outdir="$CURRENT_DIR/"; then
+    echo "============ Wheel Creation Failed for Python $PYTHON_VERSION (without isolation) ================="
+    echo "Attempting to build with isolation..."
 
     # Attempt to build the wheel without isolation
-    if ! python -m build --wheel --no-isolation --outdir="$CURRENT_DIR/"; then
-        echo "============ Wheel Creation Failed for Python $PYTHON_VERSION (without isolation) ================="
+    if ! python -m build --wheel --outdir="$CURRENT_DIR/"; then
+        echo "============ Wheel Creation Failed for Python $PYTHON_VERSION ================="
         cleanup "$VENV_DIR"
         [ -n "$TEMP_BUILD_SCRIPT_PATH" ] && rm "$CURRENT_DIR/$TEMP_BUILD_SCRIPT_PATH"
         exit 1
